c3669facc1796a8d0e2288fde0ab67246840ce38
Conor Ladrigan <Conorladrigan95@gmail.com>
2021-04-29 02:21:42 +0100

    added linting

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const { request, response } = require('express')
 require('dotenv').config()
 const express = require('express')
@@ -13,99 +15,99 @@ app.use(express.static('build'))
 morgan.token('body',  (req, res) => JSON.stringify(req.body))
 
 const morganLog = morgan(function(tokens, req, res){
-    return [
-        tokens.method(req,res),
-        tokens.url(req,res),
-        tokens.status(req,res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req,res),'ms',
-        tokens['body'](req,res)
-    ].join(' ')
+  return [
+    tokens.method(req,res),
+    tokens.url(req,res),
+    tokens.status(req,res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req,res),'ms',
+    tokens['body'](req,res)
+  ].join(' ')
 })
 
 app.use(morganLog)
 
 app.get('/api/persons', (request,response) => {
-    Contact.find({}).then(contacts => {
-        response.json(contacts)
-    })
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
 app.get('/info', (request,response) => {
-    
-    Contact.countDocuments({})
+
+  Contact.countDocuments({})
     .then(count => {
-        response.send(`<p>There are ${count} contacts in the phonebook</p>`)
+      response.send(`<p>There are ${count} contacts in the phonebook</p>`)
     })
     .catch(error =>  next(error))
 })
 
 app.put('/api/persons/:id', (request,response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const contact = {
-        name : body.name,
-        number : body.number,
-    }
+  const contact = {
+    name : body.name,
+    number : body.number,
+  }
 
-    Contact.findByIdAndUpdate(request.params.id, contact, {new:true})
+  Contact.findByIdAndUpdate(request.params.id, contact, { new:true })
     .then(updatedContact => {
-        response.json(updatedContact)
+      response.json(updatedContact)
     })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request,response,next) => {
-    
-    Contact.findById(request.params.id).then(contact => {
-        response.json(contact)
-    })
+
+  Contact.findById(request.params.id).then(contact => {
+    response.json(contact)
+  })
     .catch( error => next(error))
-})  
+})
 
 app.delete('/api/persons/:id', (request,response, next) => {
-    Contact.findByIdAndRemove(request.params.id)
+  Contact.findByIdAndRemove(request.params.id)
     .then(result => {
-        response.status(204).end()
+      response.status(204).end()
     })
     .catch( error => next(error))
 })
 
 const generateID = () => {
-    const id = Math.round(Math.random() * 1000)
-    return id
+  const id = Math.round(Math.random() * 1000)
+  return id
 }
 
 app.post('/api/persons', (request,response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const contact = new Contact ({
-        name : body.name,
-        number : body.number,
-    })
+  const contact = new Contact ({
+    name : body.name,
+    number : body.number,
+  })
 
-    contact.save().then(savedContact => {
-        response.json(savedContact)
-    })
+  contact.save().then(savedContact => {
+    response.json(savedContact)
+  })
     .catch(error => next(error))
-    
+
 })
 
 const PORT = process.env.PORT
 app.listen(PORT , () => {
-    console.log("Server Running")
+  console.log('Server Running')
 })
 
 const errorHandler = (error, request, response,next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if(error.name === "CastError"){
-        return response.status(404).send({error: 'malformed id'})
-    } else if (error.name === "ValidationError"){
-        return response.status(400).json({error:error.message})
-    }
+  if(error.name === 'CastError'){
+    return response.status(404).send({ error: 'malformed id' })
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error:error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
--- models/contact.js
+++ models/contact.js
@@ -1,30 +1,32 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 require('dotenv').config()
 const mongoose = require('mongoose')
 const uniqueValidator = require ('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
-console.log("Connecting to ", url);
+console.log('Connecting to ', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(result => {
-    console.log("connected to MongoDB");
-})
-.catch((error) => {
-    console.log("error connecting to MongoDB", error.message);
-})
-
-const contactSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    number: Number,
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
   })
 
+const contactSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  number: Number,
+})
+
 contactSchema.set('toJSON', {
-transform: (document, returnedObject) => {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-}
+  }
 })
 
 contactSchema.plugin(uniqueValidator)
